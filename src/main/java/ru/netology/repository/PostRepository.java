7a581b99aa3d1844c627a57ce9a5e9d96a16597e
Kiva Aleksey <adolfp@mail.ru>
2021-09-11 17:13:48 +0700

    Init commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,40 @@
+package ru.netology.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class PostRepository {
+  private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+  private final AtomicInteger countOfPosts = new AtomicInteger(0);
+
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    if(posts.containsKey(id)) {
+      return Optional.of(posts.get(id));
+    }
+    return Optional.empty();
+  }
+
+  public Post save(Post post) {
+    if (post.getId() == 0) {
+      post.setId(countOfPosts.incrementAndGet());
+    }
+    posts.put(post.getId(), post);
+    return post;
+  }
+
+  public void removeById(long id) {
+    posts.remove(id);
+  }
+}
